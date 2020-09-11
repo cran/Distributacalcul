9d9d7aeb75825f7a11c26aeba5477387dbb5663a
Alec James van Rassel <alec.van-rassel.1@ulaval.ca>
2020-09-11 11:00:02 +0000

    version 0.3.0

---
+++ inst/doc/distributionParameters.R
@@ -0,0 +1,18 @@
+## ----setup knitr, include = FALSE---------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+library(Distributacalcul)
+
+## ----norm-location------------------------------------------------------------
+distributacalculVis(law = "Norm", mod = "functions")
+
+## ----gamma-scale--------------------------------------------------------------
+distributacalculVis(law = "Gamma", mod = "functions")
+
+## ----rate-parameter-----------------------------------------------------------
+distributacalculVis(law = "Gamma", mod = "functions")
+
